8ab4472f20816fb3e4282b8e923fa0ad0aa865a7
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-10-15 21:03:15 +0600

    project deply

---
+++ precache-manifest.a8a0a97c758bf25d566746abcb6e6cfe.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4c0b3e7a87b145805b",
+    "url": "/aviata-test/css/app.0f7546f5.css"
+  },
+  {
+    "revision": "52d10f119696f29ca3c9",
+    "url": "/aviata-test/css/chunk-7bedda7a.5e48b739.css"
+  },
+  {
+    "revision": "cb4d4111f771e2b1db97ca0fc39fa8f3",
+    "url": "/aviata-test/img/B2.cb4d4111.png"
+  },
+  {
+    "revision": "cbeeba9d90dd7a9c3e474255eb90cd29",
+    "url": "/aviata-test/img/BT.cbeeba9d.png"
+  },
+  {
+    "revision": "4fb75867cd84631eb07267bf62d2e493",
+    "url": "/aviata-test/img/CZ.4fb75867.png"
+  },
+  {
+    "revision": "ec22854fd191f0dfc8e6d67e9bbbfd7d",
+    "url": "/aviata-test/img/DV.ec22854f.png"
+  },
+  {
+    "revision": "15f77f81582395be8ae83498e9a4d14a",
+    "url": "/aviata-test/img/EK.15f77f81.png"
+  },
+  {
+    "revision": "cc13469a66b05e602fcdd1901253a949",
+    "url": "/aviata-test/img/FZ.cc13469a.png"
+  },
+  {
+    "revision": "b412a1dc99b745847d5945ed0b8a0977",
+    "url": "/aviata-test/img/HR.b412a1dc.png"
+  },
+  {
+    "revision": "145f6685e92c0ac0a42762fd7125cca5",
+    "url": "/aviata-test/img/HY.145f6685.png"
+  },
+  {
+    "revision": "1b03ad37ed8db3b0439868a15d3c605c",
+    "url": "/aviata-test/img/LH.1b03ad37.png"
+  },
+  {
+    "revision": "71ea98e4f306c84208aaea3932c66f03",
+    "url": "/aviata-test/img/S7.71ea98e4.png"
+  },
+  {
+    "revision": "97319ae74697268d42f35399a42ddb44",
+    "url": "/aviata-test/img/SU.97319ae7.png"
+  },
+  {
+    "revision": "ec3b0e1942f936de0b7d838062bdeab1",
+    "url": "/aviata-test/img/TK.ec3b0e19.png"
+  },
+  {
+    "revision": "7252142c59b8405e318cfa92e23857bf",
+    "url": "/aviata-test/img/aviataLogo.7252142c.svg"
+  },
+  {
+    "revision": "53bcbfbe9c514898214cfbac0a245d82",
+    "url": "/aviata-test/img/icon-non-refundeble.53bcbfbe.svg"
+  },
+  {
+    "revision": "7311f1ec1182b987589300ff57101af1",
+    "url": "/aviata-test/img/refundeble.7311f1ec.svg"
+  },
+  {
+    "revision": "9139bde766ddd5548a24d4321359e0ac",
+    "url": "/aviata-test/index.html"
+  },
+  {
+    "revision": "df4c0b3e7a87b145805b",
+    "url": "/aviata-test/js/app.903411f7.js"
+  },
+  {
+    "revision": "52d10f119696f29ca3c9",
+    "url": "/aviata-test/js/chunk-7bedda7a.5b25e309.js"
+  },
+  {
+    "revision": "4b86e1be66c72f4b5877",
+    "url": "/aviata-test/js/chunk-vendors.e696c95b.js"
+  },
+  {
+    "revision": "02cb98b9a7323ff35bf3bdebe549417e",
+    "url": "/aviata-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/aviata-test/robots.txt"
+  }
+]);
